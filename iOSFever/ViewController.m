310203c20ec4a19748e8cc279c277e0d1575acee
Andras Szivak <flzhun@gmail.com>
2015-04-08 19:25:11 +0200

    project created

---
+++ iOSFever/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSFever
+//
+//  Created by Andras Szivak on 2015. 04. 08..
+//  Copyright (c) 2015. flyerz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
